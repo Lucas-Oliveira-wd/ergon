6e2dc6719f96ae9994aa87165cdb2bbe4ebbae74
Lucas-Oliveira-wd <lukasforever02@gmail.com>
2020-09-02 04:04:48 -0300

    terminado a página de projetos

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,147 +1,123 @@
-let idsli = ["projarq", "projhidros", "projhidra", "projcomb", "projelet", "projest", "projinf", "projspda"];
-function ocultar() {
-    document.getElementById(idsli[0]).style.display = "none";
-    document.getElementById(idsli[1]).style.display = "none";
-    document.getElementById(idsli[2]).style.display = "none";
-    document.getElementById(idsli[3]).style.display = "none";
-    document.getElementById(idsli[4]).style.display = "none";
-    document.getElementById(idsli[5]).style.display = "none";
-    document.getElementById(idsli[6]).style.display = "none";
-    document.getElementById(idsli[7]).style.display = "none";
-}
-function projarq() {
-    ocultar();
-    let ver = document.getElementById(idsli[0]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+const body = document.querySelector('body');
+
+// array com as classes para mostra os projetos
+let listClProj = ['show-proj-arq','show-proj-hidro','show-proj-hidra','show-proj-combinc',
+'show-proj-elet','show-proj-est','show-proj-inf','show-proj-spda'];
+
+/*==========================================================================================
+                        desta linha para baixo é para mostrar os projetos
+=========================================================================================== */
+
+// arquitetônico
+
+function sArquit (){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[0] && body.classList.contains(listClProj[0]) === true)
+        body.classList.remove(listClProj[0]);
+
+        else if (listClProj[i] === listClProj[0] && body.classList.contains(listClProj[0])!==true)
+        body.classList.add(listClProj[0]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projhidros() {
-    ocultar();
-    let ver = document.getElementById(idsli[1]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// hidrossanitario
+
+function sHidro(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[1] && body.classList.contains(listClProj[1]) === true)
+        body.classList.remove(listClProj[1]);
+
+        else if (listClProj[i] === listClProj[1] && body.classList.contains(listClProj[1])!==true)
+        body.classList.add(listClProj[1]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projhidra() {
-    ocultar();
-    let ver = document.getElementById(idsli[2]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// hidraulico
+
+function sHidra(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[2] && body.classList.contains(listClProj[2]) === true)
+        body.classList.remove(listClProj[2]);
+
+        else if (listClProj[i] === listClProj[2] && body.classList.contains(listClProj[2])!==true)
+        body.classList.add(listClProj[2]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projcomb() {
-    ocultar();
-    let ver = document.getElementById(idsli[3]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// Combate a incendio
+
+function sCombInc(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[3] && body.classList.contains(listClProj[3]) === true)
+        body.classList.remove(listClProj[3]);
+
+        else if (listClProj[i] === listClProj[3] && body.classList.contains(listClProj[3])!==true)
+        body.classList.add(listClProj[3]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projelet() {
-    ocultar();
-    let ver = document.getElementById(idsli[4]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// eletrico
+
+function sElet(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[4] && body.classList.contains(listClProj[4]) === true)
+        body.classList.remove(listClProj[4]);
+
+        else if (listClProj[i] === listClProj[4] && body.classList.contains(listClProj[4])!==true)
+        body.classList.add(listClProj[4]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projest() {
-    ocultar();
-    let ver = document.getElementById(idsli[5]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// Estrutural
+
+function sEst(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[5] && body.classList.contains(listClProj[5]) === true)
+        body.classList.remove(listClProj[5]);
+
+        else if (listClProj[i] === listClProj[5] && body.classList.contains(listClProj[5])!==true)
+        body.classList.add(listClProj[5]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projinf() {
-    ocultar();
-    let ver = document.getElementById(idsli[6]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[7]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// Inferiores
+
+function sInf(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[6] && body.classList.contains(listClProj[6]) === true)
+        body.classList.remove(listClProj[6]);
+
+        else if (listClProj[i] === listClProj[6] && body.classList.contains(listClProj[6])!==true)
+        body.classList.add(listClProj[6]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
-function projspda() {
-    ocultar();
-    let ver = document.getElementById(idsli[7]);
-    ver.style.display='block'
-    if(ver.classList.contains('activeinmoment') == true){
-        ver.classList.remove('activeinmoment');
-    document.getElementById(idsli[0]).style.display = "block";
-    document.getElementById(idsli[1]).style.display = "block";
-    document.getElementById(idsli[2]).style.display = "block";
-    document.getElementById(idsli[3]).style.display = "block";
-    document.getElementById(idsli[4]).style.display = "block";
-    document.getElementById(idsli[5]).style.display = "block";
-    document.getElementById(idsli[6]).style.display = "block";
-    } else {
-        ver.classList.add('activeinmoment');
+
+// SPDA
+
+function sSPDA(){
+    for(let i in listClProj){
+        if (listClProj[i] === listClProj[7] && body.classList.contains(listClProj[7]) === true)
+        body.classList.remove(listClProj[7]);
+
+        else if (listClProj[i] === listClProj[7] && body.classList.contains(listClProj[7])!==true)
+        body.classList.add(listClProj[7]);
+
+        else body.classList.remove(listClProj[i]);
     }
 }
+
+
